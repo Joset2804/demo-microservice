ab977bbefd06d871d7b006ae687295df0913d52a
Jose Tarazona <joset2804@hotmail.com>
2022-07-03 22:53:46 -0500

    .

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (id, created_at, updated_at, first_name) VALUES (1, '2021-07-03 22:33:03.034', '2021-07-03 22:33:03.034','prueba 1');
+
+INSERT INTO book (id, created_at, updated_at, name, user_id) VALUES (1, '2021-07-03 22:33:03.034', '2021-07-03 22:33:03.034','qwerty 1', 1);
